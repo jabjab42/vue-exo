4111cd67e375df6b339f15113e04386ed66da56f
jabjab42 <julien314314@gmail.com>
2024-11-14 09:01:33 +0100

    étape 6

---
+++ vue-exo/src/components/BasicInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <input v-model="inputValue" :type="type" :name="name" :id="name" :required="required">
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  name: String,
+  type: String,
+  required: Boolean
+})
+
+const inputValue = ref('');
+
+</script>
